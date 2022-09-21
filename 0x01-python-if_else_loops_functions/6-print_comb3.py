1bb64718b9261b9e1c81bbbc4ac6f49cad0bc6a6
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-09-21 06:57:14 -0700

    fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-	print(
-	    "{}{}".format(i, j),
-	    end=", " if int(str(i) + str(j)) < 89 else "\n"
-	    )
+	print("{}{}".format(i, j),end=", " if int(str(i) + str(j)) < 89 else "\n")

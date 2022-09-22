f7aa59ee0fe4b3f8711c287b2ef97e9640f66560
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-09-22 05:34:31 -0700

    fixed

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add:
 a = 1
 b = 2
-add(a, b)
-    print("<{} value> + <{} value> = <add({}, {}>)".format(a, b, a, b), end="")
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))

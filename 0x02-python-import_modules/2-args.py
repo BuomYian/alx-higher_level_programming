328dbc8eb461f32ae674f75ee834c88433feac5c
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-09-22 06:58:12 -0700

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
         print("0 arguments.")
     else:
         if total == 2:
-	    print("{:d} argument:".format(total - 1))
-	else:
-	    print("{:d} arguments:".format(total - 1))
-	for i in range(1, total):
-	    print("{:d}: {}".format(i, sys.argv[i]))
+            print("{:d} argument:".format(total - 1))
+        else:
+            print("{:d} arguments:".format(total - 1))
+        for i in range(1, total):
+            print("{:d}: {}".format(i, sys.argv[i]))

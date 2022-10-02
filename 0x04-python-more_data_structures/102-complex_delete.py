7c83695cd5fa70b981f07c8559723550ebaa9240
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-10-02 10:27:10 -0700

    del keys

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-def complex_delete(my_dict, value):
-    tmp = my_dict.copy()
-    for k, v in tmp.items():
-        if value == v:
-            my_dict.pop(k)
-    return my_dict
+def complex_delete(a_dictionary, value):
+    keys_to_del = []
+    for key in a_dictionary:
+        if a_dictionary[key] == value:
+            keys_to_del.append(key)
+    for key in keys_to_del:
+        del a_dictionary[key]
+    return a_dictionary

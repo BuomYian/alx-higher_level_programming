bf45c4b78697e7518f6c35a55fca9703894a6607
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-10-12 08:13:04 -0700

    added file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-square.py by Buomkuoth Yian
+"""Defines a square """
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - represents the size of the square defined
+        """
+
+        self.__size = size

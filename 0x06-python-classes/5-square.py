28cba679f84f1ed9b6caea63aa576f332e66e228
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-10-12 09:20:49 -0700

    fixed

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -43,3 +43,12 @@ class Square:
         """
 
         return (self.__size ** 2)
+
+    def my_print(self):
+        """print the square in # """
+
+        if self.__size == 0:
+            print()
+
+        for i in range(self.__size):
+            print("#" * self.__size)

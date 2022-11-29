c00a8f5b0c1c964b200eaf63f832a7de63d29ebf
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-11-29 05:01:37 -0800

    order by score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- script that lists all records of the table
+-- Results should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
+-- The database name will be passed as an argument of the mysql command
+SELECT score, name FROM second_table ORDER BY score DESC;

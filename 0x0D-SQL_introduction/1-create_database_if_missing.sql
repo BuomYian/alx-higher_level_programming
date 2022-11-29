eb1900b583e673a0229b0c1f8bd55198fda8616d
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-11-29 04:00:26 -0800

    added files

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- script to create a database
+-- does not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- script to delete database
+-- does not fail
+DELETE DATABASE IF EXISTS hbtn_0c_0;

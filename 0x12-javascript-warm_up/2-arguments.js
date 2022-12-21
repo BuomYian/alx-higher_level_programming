d2f8508ff710bf03e6f8ded8e8f3793ed9be6ca6
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-12-21 10:27:19 -0800

    fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,4 +3,4 @@
 // Script to print the number of arguments
 
 const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument fount' : 'Arguments found');
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');

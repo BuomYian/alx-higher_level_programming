408566e7f4ed40fad950c0c8fc958e0c33b66549
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-12-25 08:47:36 -0800

    x times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Script that print x times 'C is fun'
+
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}

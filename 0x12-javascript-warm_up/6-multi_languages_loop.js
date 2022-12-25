f4b4c040d5bbe467e079483993fee50c5106b81a
Buomkuoth Yian <buom.nyajal@gmail.com>
2022-12-25 08:29:22 -0800

    array of string

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Script that print lines of string
+
+let i;
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (i = 0; i < 3; i++) {
+  console.log(myString[i]);
+}
